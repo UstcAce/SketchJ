39bb2f4c8d2e79cc61e96fbdefea4ff0a68f28e0
Min Wang <ustcace@mail.ustc.edu.cn>
2020-03-04 21:03:59 +0800

    feat: 新增Q44字符串匹配问题动态规划解法

--- oj/src/main/java/leetcode/Q44WildCardMatch.java
+++ oj/src/main/java/leetcode/Q44WildCardMatch.java
@@ -78,6 +78,48 @@ public class Q44WildCardMatch {
         return p.replaceAll(pattern, "\\*");
     }
 
+    /**
+     * 解法2：DP
+     * 1. 问题定义 dp[i][j]表示s的前i个字符和p的前j个字符匹配的关系
+     * 2. 状态转移方程
+     *   dp[i][j] =
+     *   (1) 若s[i] == p[j]或者p[j]=='?'; dp[i-1][j-1]
+     *   (2) 若p[j] == '*'; dp[i][j-1] || dp[i-1][j]
+     *   dp[i][j-1] 表示此星号不匹配字符
+     *   dp[i-1][j] 表示此星号至少匹配一个字符
+     *   (3)其他情况false
+     * 3. 边界条件
+     * (1)dp[0][0] = true
+     * (2)dp[0][j],0<j<=lenP, 只有在p的前j个字符都是'*'才为true，其他情况都是false
+     * (3)dp[i][0] = false,0<i<=lenS
+     */
+    public boolean isMatch2(String s, String p) {
+        p = cleanDuplicateStar(p);
+        int lenS = s.length();
+        int lenP = p.length();
+
+        boolean [][] dp = new boolean[lenS+1][lenP+1];
+
+        dp[0][0] = true;
+
+        for (int i=1; i<=lenP; i++) {
+            dp[0][i] = cleanDuplicateStar(p.substring(0, i)).equals("*");
+        }
+
+        for (int i=1; i<=lenS; i++) {
+            for (int j=1; j<=lenP; j++) {
+                if (s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1)=='?') {
+                    dp[i][j] = dp[i-1][j-1];
+                } else if (p.charAt(j-1)=='*') {
+                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[lenS][lenP];
+    }
+
     private static class KeyStr {
         public String a;
         public String b;
@@ -156,14 +198,14 @@ public class Q44WildCardMatch {
     public void testCase03() {
         String s = "ho";
         String p = "ho**";
-        System.out.println(isMatch(s, p));
+        System.out.println(isMatch2(s, p));
     }
 
     @Test
     public void testCase04() {
         String s = "aa";
         String p = "a";
-        System.out.println(isMatch(s, p));
+        System.out.println(isMatch2(s, p));
     }
 
     @Test
