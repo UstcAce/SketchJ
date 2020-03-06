326b853c490d2e60b86225243b3971a52eb029b4
Min Wang <ustcace@mail.ustc.edu.cn>
2020-03-06 20:55:00 +0800

    feat: 新增Q138深拷贝带随机指针链表一次迭代+缓存解法

--- oj/src/main/java/leetcode/Q138CopyListWithRandomPointer.java
+++ oj/src/main/java/leetcode/Q138CopyListWithRandomPointer.java
@@ -3,7 +3,9 @@ package leetcode;
 import common.Node;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Q138CopyListWithRandomPointer {
     public Node copyRandomList(Node head) {
@@ -38,5 +40,41 @@ public class Q138CopyListWithRandomPointer {
         }
 
         return newNodes.iterator().next();
+    }
+
+    public Node copyRandomList2(Node head) {
+        if (head == null) {
+            return null;
+        }
+
+        Map<Node, Node> map = new HashMap<>();
+        Node iter = head;
+        Node newNode = new Node(head.val);
+        map.put(iter, newNode);
+        while (iter != null) {
+            Node random = getClonedNode(iter.random, map);
+            Node next = getClonedNode(iter.next, map);
+
+            newNode.next = next;
+            newNode.random = random;
+
+            iter = iter.next;
+            newNode = next;
+        }
+        return map.get(head);
+    }
+
+    private Node getClonedNode(Node node, Map<Node, Node> map) {
+        if (node == null) {
+            return null;
+        }
+
+        if (map.containsKey(node)) {
+            return map.get(node);
+        } else {
+            Node newNode = new Node(node.val);
+            map.put(node, newNode);
+            return newNode;
+        }
     }
 }
