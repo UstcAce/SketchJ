7964849ed25fb677a71e6cf5d8c5961e5efb99ce
Min Wang <ustcace@mail.ustc.edu.cn>
2020-03-23 21:28:42 +0800

    feat: 新增Q669修剪二叉树题解

---
+++ oj/src/main/java/leetcode/Q669TrimBST.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import common.TreeNode;
+
+public class Q669TrimBST {
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if (root == null) return null;
+
+        if (root.val >= L && root.val <= R) {
+            root.left = trimBST(root.left, L, R);
+            root.right = trimBST(root.right, L, R);
+        } else if (root.val < L) {
+            return trimBST(root.right, L, R);
+        } else {  // root.val > R
+            return trimBST(root.left, L, R);
+        }
+
+        return root;
+    }
+}
