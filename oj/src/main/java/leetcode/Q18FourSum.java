368a8b10503f06f7b0dafd34506f971340282b4c
Min Wang <ustcace@mail.ustc.edu.cn>
2020-03-29 17:12:14 +0800

    feat: 新增Q18四数之和题解

---
+++ oj/src/main/java/leetcode/Q18FourSum.java
@@ -0,0 +1,64 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，
+ * 使得 a + b + c + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
+ * 注意：
+ * 答案中不可以包含重复的四元组。
+ *
+ * 示例：
+ * 给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
+ * 满足要求的四元组集合为：
+ * [
+ *   [-1,  0, 0, 1],
+ *   [-2, -1, 1, 2],
+ *   [-2,  0, 0, 2]
+ * ]
+ *
+ */
+public class Q18FourSum {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        int len = nums.length;
+        if (len < 4) return res;
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < len - 1; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < len - 1; j++) {
+                if (j > 0 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int left = j + 1;
+                int right = len - 1;
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum < target) {
+                        left++;
+                    } else if (sum > target) {
+                        right--;
+                    } else {
+                        List<Integer> oneSolution = Arrays.asList(nums[i], nums[j], nums[left], nums[right]);
+                        res.add(oneSolution);
+                        while (left + 1 < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        while (left < right - 1 && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
