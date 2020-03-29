4a91b9f3f67616c518ade184ccc9f7ab378c0f86
Min Wang <ustcace@mail.ustc.edu.cn>
2020-03-29 22:06:15 +0800

    feat: 新增Q35搜索指定插入元素位置题解

---
+++ oj/src/main/java/leetcode/Q35SearchInsert.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+/**
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * 你可以假设数组中无重复元素。
+ *
+ * 示例 1:
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ *
+ * 示例 2:
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ *
+ * 示例 3:
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ *
+ * 示例 4:
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ */
+public class Q35SearchInsert {
+    /**
+     *   此题类似于在一个排序数组中找第一个等于target元素的下表
+     *   1. 搜索区间为[left, right), 左闭右开
+     *   2. 迭代停止条件为 left == right
+     *   3. 最终返回的下标left表示nums数组中有多少值小于target
+     */
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                right = mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+}
