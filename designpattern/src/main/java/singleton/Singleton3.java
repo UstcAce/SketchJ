6e4143de77489fb258a7f9ff21b69a77acb8c261
Min Wang <ustcace@mail.ustc.edu.cn>
2020-06-13 11:46:40 +0800

    docs: 增加单例实现模式3的注释

--- designpattern/src/main/java/singleton/Singleton3.java
+++ designpattern/src/main/java/singleton/Singleton3.java
@@ -3,7 +3,8 @@ package singleton;
 /**
  * 懒汉式，线程安全
  * 通过类锁保证多线程并发调用
- * 缺点：必须加锁 synchronized 才能保证单例，但加锁会影响效率。
+ * 缺点：必须加锁 synchronized 才能保证单例，效率很低，99% 情况下不需要同步。
+ * 100个线程调用getInstance()，类锁要求它们逐个执行。但是如果instance非空的情况下，不需要加锁。
  * getInstance() 的性能对应用程序不是很关键（该方法使用不太频繁）。
  */
 public class Singleton3 {
