1b795fd82adc6610e8fd52f8d481360282f724f0
Min Wang <ustcace@mail.ustc.edu.cn>
2020-06-17 21:35:38 +0800

    feat: 新增二叉树的非递归后序遍历

--- oj/src/main/java/common/TreeNode.java
+++ oj/src/main/java/common/TreeNode.java
@@ -1,5 +1,9 @@
 package common;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class TreeNode {
     public int val;
     public TreeNode left;
@@ -16,5 +20,38 @@ public class TreeNode {
                 ", left=" + left +
                 ", right=" + right +
                 '}';
+    }
+
+    /**
+     * 二叉树的非递归后序遍历：左-右-根
+     * 遍历时需要判断是从左子树回到根节点还是从右子树回到根节点
+     * (1) 若当前节点右子树为空 或者 上一个遍历节点是当前节点的右子树，则弹出当前节点
+     * (2) 否则迭代节点=当前节点的右子树
+     */
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        TreeNode iter = root;
+        TreeNode last = null;
+        Stack<TreeNode> stack = new Stack<>();
+        while (iter != null || !stack.isEmpty()) {
+            while (iter != null) {
+                stack.add(iter);
+                iter = iter.left;
+            }
+            TreeNode node = stack.peek();
+            if (node.right == null || node.right == last) {
+                stack.pop();
+                result.add(node.val);
+                last = node;
+            } else {
+                iter = node.right;
+            }
+        }
+
+        return result;
     }
 }
