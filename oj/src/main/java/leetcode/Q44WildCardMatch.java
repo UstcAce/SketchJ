2ad4acebca9573b915aaddefb8b8cafee91f1cbd
Min Wang <ustcace@mail.ustc.edu.cn>
2020-07-05 21:10:40 +0800

    feat: 重写Q44通配符匹配问题

--- oj/src/main/java/leetcode/Q44WildCardMatch.java
+++ oj/src/main/java/leetcode/Q44WildCardMatch.java
@@ -1,10 +1,7 @@
 package leetcode;
 
+import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
 
 /**
  * 给定一个字符串 (s) 和一个字符模式 (p) ，实现一个支持 '?' 和 '*' 的通配符匹配。
@@ -17,60 +14,43 @@ import java.util.Objects;
  *
  * s 可能为空，且只包含从 a-z 的小写字母。
  * p 可能为空，且只包含从 a-z 的小写字母，以及字符 ? 和 *。
- *
  */
 public class Q44WildCardMatch {
-    private Map<KeyStr, Boolean> map = new HashMap<KeyStr, Boolean>();
     /**
-     * 解法1：递归 + 记忆 + 清除冗余star
+     * 1. 定义动态规划求解问题 dp[i][j]表示s的前i个字符是否和p的前j个字符匹配
+     * 2. 状态转移方程：
+     * (1) s[i] == p[j] || p[j] == '?'
+     * dp[i][j] = dp[i-1][j-1]
+     * (2) p[j] == '*'
+     * (2.1) *匹配0个字符，dp[i][j] = dp[i][j-1]
+     * (2.2) *至少匹配1个字符，dp[i][j] = dp[i-1][j]
+     * 3. 边界条件
+     * dp[0][0] = true
+     * dp[i][0] = false, i > 0, p = "",无法匹配非空s子串
+     * dp[0][j] = p.subString(0, j) == "*"
      */
     public boolean isMatch(String s, String p) {
         p = cleanDuplicateStar(p);
-
-        KeyStr keyStr = new KeyStr(s, p);
-        if (map.containsKey(keyStr)) {
-            return map.get(keyStr);
+        int sLen = s.length();
+        int pLen = p.length();
+        boolean[][] dp = new boolean[sLen + 1][pLen + 1];
+        dp[0][0] = true;
+        for (int j = 1; j <= pLen; j++) {
+            dp[0][j] = p.substring(0, j).equals("*");
         }
-
-        if (s.equals(p)) {
-            map.put(keyStr, true);
-            return true;
+        for (int i = 1; i <= sLen; i++) {
+            for (int j = 1; j <= pLen; j++) {
+                char sChar = s.charAt(i - 1);
+                char pChar = p.charAt(j - 1);
+                if (sChar == pChar || pChar == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pChar == '*') {
+                    // *匹配0个字符 || *匹配至少一个字符
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                }
+            }
         }
-        if (p.equals("*")) {
-            map.put(keyStr, true);
-            return true;
-        }
-        if (s.equals("")) {
-            boolean res = p.replace("*", "").equals("");
-            map.put(keyStr, res);
-            return res;
-        }
-
-        if (p.equals("")) {
-            map.put(keyStr, false);
-            return false;
-        }
-
-        String s0 = s.substring(0, 1);
-        String p0 = p.substring(0, 1);
-
-        if (s0.equals(p0) || p0.equals("?")) {
-            boolean res = isMatch(s.substring(1), p.substring(1));
-            map.put(keyStr, res);
-            return res;
-        }
-
-        if (p0.equals("*")) {
-            // 星号没有匹配字符
-            boolean cond1 = isMatch(s, p.substring(1));
-            // 星号至少匹配了一个字符
-            boolean cond2 = isMatch(s.substring(1), p);
-            boolean res = cond1 || cond2;
-            map.put(keyStr, res);
-            return res;
-        }
-        map.put(keyStr, false);
-        return false;
+        return dp[sLen][pLen];
     }
 
     private String cleanDuplicateStar(String p) {
@@ -120,30 +100,6 @@ public class Q44WildCardMatch {
         return dp[lenS][lenP];
     }
 
-    private static class KeyStr {
-        public String a;
-        public String b;
-
-        public KeyStr(String a, String b) {
-            this.a = a;
-            this.b = b;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            KeyStr keyStr = (KeyStr) o;
-            return a.equals(keyStr.a) &&
-                    b.equals(keyStr.b);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(a, b);
-        }
-    }
-
     /**
      * 示例 1:
      *
@@ -168,13 +124,13 @@ public class Q44WildCardMatch {
      * 解释: '?' 可以匹配 'c', 但第二个 'a' 无法匹配 'b'。
      * 示例 4:
      *
-     * 输入:
+     *  输入:
      * s = "adceb"
      * p = "*a*b" ,"","a","b"
      * 输出: true
      * 解释: 第一个 '*' 可以匹配空字符串, 第二个 '*' 可以匹配字符串 "dce".
-     * 示例 5:
      *
+     *  示例 5:
      * 输入:
      * s = "acdcb"
      * p = "a*c?b"
@@ -184,34 +140,53 @@ public class Q44WildCardMatch {
     public void testCase01() {
         String s = "acdcb";
         String p = "*a*b";
-        System.out.println(isMatch(s, p));
+        boolean res = isMatch(s, p);
+        boolean excepted = true;
+        Assert.assertEquals(excepted, res);
     }
 
     @Test
     public void testCase02() {
         String s = "acdcb";
         String p = "a*c?b";
-        System.out.println(isMatch(s, p));
+        boolean res = isMatch(s, p);
+        boolean excepted = false;
+        Assert.assertEquals(excepted, res);
     }
 
     @Test
     public void testCase03() {
         String s = "ho";
         String p = "ho**";
-        System.out.println(isMatch2(s, p));
+        boolean res = isMatch(s, p);
+        boolean excepted = true;
+        Assert.assertEquals(excepted, res);
+    }
+
+    @Test
+    public void testCase03_2() {
+        String s = "ho";
+        String p = "**ho";
+        boolean res = isMatch(s, p);
+        boolean excepted = true;
+        Assert.assertEquals(excepted, res);
     }
 
     @Test
     public void testCase04() {
         String s = "aa";
         String p = "a";
-        System.out.println(isMatch2(s, p));
+        boolean res = isMatch(s, p);
+        boolean excepted = false;
+        Assert.assertEquals(excepted, res);
     }
 
     @Test
     public void testCase05() {
         String s = "abbabbbaabaaabbbbbabbabbabbbabbaaabbbababbabaaabbab";
         String p = "*aabb***aa**a******aa*";
-        System.out.println(isMatch(s, p));
+        boolean res = isMatch(s, p);
+        boolean excepted = true;
+        Assert.assertEquals(excepted, res);
     }
 }
