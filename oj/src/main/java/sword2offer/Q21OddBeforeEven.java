dfc5b9c667b780a0be01f33842b916ca458c92a4
Min Wang <ustcace@mail.ustc.edu.cn>
2020-07-06 23:16:01 +0800

    feat: 新增Q21将奇数调整至偶数之前

---
+++ oj/src/main/java/sword2offer/Q21OddBeforeEven.java
@@ -0,0 +1,29 @@
+package sword2offer;
+
+/**
+ * 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有奇数位于数组的前半部分，所有偶数位于数组的后半部分。
+ *
+ * 示例：
+ * 输入：nums = [1,2,3,4]
+ * 输出：[1,3,2,4]
+ * 注：[3,1,2,4] 也是正确的答案之一。
+ *
+ */
+public class Q21OddBeforeEven {
+    public int[] exchange(int[] nums) {
+        int oddNum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 1) {
+                swap(nums, i, oddNum);
+                oddNum++;
+            }
+        }
+        return nums;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
