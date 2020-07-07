03f3053366390481c1f7131f2cc719feae90cccf
Min Wang <ustcace@mail.ustc.edu.cn>
2020-07-07 09:23:25 +0800

    refactor: 更新Q346移动数据流中的平均值题解

--- oj/src/main/java/leetcode/Q346MovingAverage.java
+++ oj/src/main/java/leetcode/Q346MovingAverage.java
@@ -2,14 +2,25 @@ package leetcode;
 
 import java.util.LinkedList;
 
-public class MovingAverage {
+/**
+ * 给定一个整数数据流和一个窗口大小，根据该滑动窗口的大小，计算其所有整数的移动平均值。
+ *
+ * 示例:
+ *
+ * MovingAverage m = new MovingAverage(3);
+ * m.next(1) = 1
+ * m.next(10) = (1 + 10) / 2
+ * m.next(3) = (1 + 10 + 3) / 3
+ * m.next(5) = (10 + 3 + 5) / 3
+ */
+public class Q346MovingAverage {
     private int size;
 
     private LinkedList<Integer> queue;
 
     private int sum;
     /** Initialize your data structure here. */
-    public MovingAverage(int size) {
+    public Q346MovingAverage(int size) {
         this.size = size;
         queue = new LinkedList<>();
         sum = 0;
